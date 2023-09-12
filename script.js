faaa24a9af69988079d31584e47180ba522d2218
alamshukla <33272550+alamshukla@users.noreply.github.com>
2023-09-12 07:16:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1185,7 +1185,9 @@
             var fullScreen = doc.fullScreen || doc.mozFullScreen || doc.webkitIsFullScreen || doc.msFullscreenElement || Boolean(doc.webkitFullscreenElement);
             $(brandElement).attr("style", fullScreen ? "display: none !important;" : "");
           }
-          function createBadge() {}
+          function createBadge() {
+              return;
+          }
           function ensureBrand() {
             var found = $body.children(namespace);
             var match = found.length && found.get(0) === brandElement;
